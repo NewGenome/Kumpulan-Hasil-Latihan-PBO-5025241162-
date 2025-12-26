d182a8741d20bd52845c5c3ac7660686146632c8
NewGenome <crisisgear0@gmail.com>
2025-12-26 14:15:24 +0700

    Update Config.java

--- PBO 15 - CRUD Database/Config.java
+++ PBO 15 - CRUD Database/Config.java
@@ -7,8 +7,8 @@ public class Config {
     public static Connection configDB() throws SQLException {
         try {
             String url = "jdbc:mysql://localhost:3306/perpustakaan"; // url database
-            String user = "root"; // user database
-            String pass = ""; // password database
+            String user = "root"; 
+            String pass = "";
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
             mysqlconfig = DriverManager.getConnection(url, user, pass);            
         } catch (Exception e) {
